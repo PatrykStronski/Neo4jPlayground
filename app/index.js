3f3ad4344e3dbe78a5db783f1fc187d5add7ed98
Patryk Stronski <patryk.stronsky.extern@joyn.de>
2021-05-11 01:04:00 +0200

    works

--- app/index.js
+++ app/index.js
@@ -1,6 +1,10 @@
 'use strict';
 
 const Hapi = require('@hapi/hapi');
+const neo4j = require('neo4j-driver');
+
+const driver = neo4j.driver('neo4j://neo_dest/neo4j', neo4j.auth.basic('neo4j', 'test'));
+const session = driver.session();
 
 const init = async () => {
 
@@ -9,14 +13,56 @@ const init = async () => {
         host: 'localhost'
     });
 
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-
-            return 'Hello World!';
-        }
+    server.route({ 
+			method: 'GET', 
+			path: '/', 
+			handler: (request, h) => {
+				return 'Hello Neo4j!';
+      }
     });
+
+		server.route({ 
+			method: 'GET',
+			path: '/all_actors',
+			handler: async (request, h) => {
+				try {
+					const result = await session.run('MATCH (n:Person) RETURN n.name as name');
+					const actors = result.records.map(rec => rec.get('name'));
+					return actors;
+				} catch (e) {
+					return h.response('Sth went wrong').code(500);
+				}
+			}
+		});
+
+		server.route({ 
+			method: 'POST',
+			path: '/actors_filter_born',
+			handler: async (request, h) => {
+				try {
+					const payload = request.payload;
+					const result = await session.run(`MATCH (n:Person) WHERE n.born = ${payload.born} RETURN n.name as name`);
+					const actors = result.records.map(rec => rec.get('name'));
+					return actors;
+				} catch (e) {
+					return h.response('Sth went wrong').code(500);
+				}
+			}
+		});
+
+		server.route({ 
+			method: 'GET',
+			path: '/all_actors_nmb',
+			handler: async (request, h) => {
+				try {
+					const result = await session.run('MATCH (n:Person) RETURN COUNT(n) AS qty');
+					const numberOfActors = result.records[0].get('qty');
+					return numberOfActors;
+				} catch (e) {
+					return h.response('Sth went wrong').code(500);
+				}
+			}
+		});
 
     await server.start();
     console.log('Server running on %s', server.info.uri);
